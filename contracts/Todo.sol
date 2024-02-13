3130a891a2ffaedfbc42c602f09f652beaa13bd1
panshak <panshakeden@gmail.com>
2024-02-13 16:42:24 +0100

    get  todo commit

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -23,6 +23,28 @@ struct MyTodo{
     }));
  }
 
+ function getTodo()public view returns(MyTodo[] memory){
+    return todos;
+ }
+
+ function ToogleTodoIsDone(uint256 _index)public {
+    todos[_index].IsDone =! todos[_index].IsDone;
+   
+ }
+
+//  function UpdateTodos(uint256 _id,string memory _name, string memory _title, string memory _description, bool _IsDone)public  {
+//     todos[_id]=MyTodo({
+//         name:_name,
+//         title:_title,
+//         description:_description,
+//         IsDone:_IsDone
+//     });
+//  }
+
+//  function DeleteArray(uint _id) public {
+//     delete todos[_id];
+    
+//  }
 
 
 
