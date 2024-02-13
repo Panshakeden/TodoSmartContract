33aa5379db0ff65256e902ea7b07b4b3d7fa1f8a
panshak <panshakeden@gmail.com>
2024-02-13 16:41:35 +0100

    second commit

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -11,40 +11,18 @@ struct MyTodo{
      bool  IsDone;
 }
 
-//  MyTodo[] public  todos;
+ MyTodo[] public  todos;
 
-//  function createTodo(string memory _name, string memory _title,string  memory _description, bool _IsDone)public  {
-//     todos.push(MyTodo({
-//         name:_name,
-//         title:_title,
-//         description:_description,
-//         IsDone:_IsDone
+ function createTodo(string memory _name, string memory _title,string  memory _description, bool _IsDone)public  {
+    todos.push(MyTodo({
+        name:_name,
+        title:_title,
+        description:_description,
+        IsDone:_IsDone
 
-//     }));
-//  }
+    }));
+ }
 
-//  function getTodo()public view returns(MyTodo[] memory){
-//     return todos;
-//  }
-
-//  function ToogleTodoIsDone(uint256 _index)public {
-//     todos[_index].IsDone =! todos[_index].IsDone;
-   
-//  }
-
-//  function UpdateTodos(uint256 _id,string memory _name, string memory _title, string memory _description, bool _IsDone)public  {
-//     todos[_id]=MyTodo({
-//         name:_name,
-//         title:_title,
-//         description:_description,
-//         IsDone:_IsDone
-//     });
-//  }
-
-//  function DeleteArray(uint _id) public {
-//     delete todos[_id];
-    
-//  }
 
 
 
